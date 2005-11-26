c163de24e871a1bfc9b5c149abdca236fceee320
pcman <pcman@ac1be623-90ea-0310-9410-ba68b2efa777>
2005-11-26 15:12:00 +0000

    made a copy
    git-svn-id: https://svn.csie.net/chewing/win32-chewing@470 ac1be623-90ea-0310-9410-ba68b2efa777

---
+++ HashEd-UTF8/HashEd.h
@@ -0,0 +1,25 @@
+// HashEd.h : main header file for the HASHED application
+//
+
+#if !defined(AFX_HASHED_H__69C59549_6152_4802_A835_892AD84C996D__INCLUDED_)
+#define AFX_HASHED_H__69C59549_6152_4802_A835_892AD84C996D__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "stdafx.h"
+
+#include "resource.h"		// main symbols
+
+/////////////////////////////////////////////////////////////////////////////
+// CHashEdApp:
+// See HashEd.cpp for the implementation of this class
+//
+
+/////////////////////////////////////////////////////////////////////////////
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_HASHED_H__69C59549_6152_4802_A835_892AD84C996D__INCLUDED_)
---
+++ HashEd-UTF8/resource.h
@@ -0,0 +1,40 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by HashEd.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_HASHED_DIALOG               102
+#define IDS_ALLOW_CHI_STRING            102
+#define IDS_INPUT_RULE                  103
+#define IDS_PHRASE_EXIST                104
+#define IDS_SAVE_CONFIRM                105
+#define IDS_SAVE_BTN_FACE               106
+#define IDS_NUM_OF_PHRASES              107
+#define IDS_LOADING                     108
+#define IDS_SUGGEST_DO_SAVE             109
+#define IDB_COOL                        122
+#define IDR_MAINFRAME                   128
+#define IDC_PHRASE_LIST                 1001
+#define IDC_NEW_PHRASE_EDIT             1002
+#define IDC_ADD_PHRASE                  1003
+#define IDC_DEL_PHRASE                  1004
+#define IDC_FIND_PHRASE                 1005
+#define IDC_IMPORT                      1006
+#define IDC_IMPORT2                     1007
+#define IDC_SAVE                        1007
+#define IDC_SAVE_AS                     1008
+#define IDC_ABOUT                       1009
+#define IDC_BANNER                      1010
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
