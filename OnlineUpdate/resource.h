321a75c85d6879c1c9aa302b0758a4c5861a1a83
pcman <pcman@ac1be623-90ea-0310-9410-ba68b2efa777>
2006-05-14 14:56:18 +0000

    Add an update notifier which checks new versions on the official website.
    git-svn-id: https://svn.csie.net/chewing/win32-chewing@622 ac1be623-90ea-0310-9410-ba68b2efa777

---
+++ OnlineUpdate/resource.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by OnlineUpdate.rc
+//
+#define IDD_POPUP                       101
+#define IDR_RT_MANIFEST1                102
+#define IDC_CHANGELOG                   1001
+#define IDC_WEBPAGE                     1002
+#define IDC_VERSION                     1003
+#define IDC_TITLE                       1004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
