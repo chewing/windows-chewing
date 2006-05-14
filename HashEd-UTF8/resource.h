ab1358941f1a75d6daa4c588e45338ca8d887d7c
pcman <pcman@ac1be623-90ea-0310-9410-ba68b2efa777>
2006-05-14 15:14:43 +0000

    Add xp visual style (via manifest) to Hash Editor.
    git-svn-id: https://svn.csie.net/chewing/win32-chewing@625 ac1be623-90ea-0310-9410-ba68b2efa777

--- HashEd-UTF8/resource.h
+++ HashEd-UTF8/resource.h
@@ -17,6 +17,7 @@
 #define IDS_NOTFOUND                    110
 #define IDB_COOL                        122
 #define IDR_MAINFRAME                   128
+#define IDR_RT_MANIFEST1                129
 #define IDC_PHRASE_LIST                 1001
 #define IDC_NEW_PHRASE_EDIT             1002
 #define IDC_ADD_PHRASE                  1003
@@ -35,7 +36,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1016
 #define _APS_NEXT_SYMED_VALUE           101
